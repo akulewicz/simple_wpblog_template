e974c887bd01fdaf9c1be28cc2d725364ec69e73
Arkadiusz Kulewicz <a.kulewicz@gmail.com>
2021-02-13 10:00:52 +0100

    Delete unnecessary code

--- front-page.php
+++ front-page.php
@@ -11,19 +11,7 @@
             <p class="post__date"><?php the_date(); ?></p>
         </header>
         <?php the_excerpt(); ?>
-
     </article>
-            <!-- <article class="short-post"> 
-                <header class="short-post__header">
-                    <h2 class="short-post__title"><a class="short-post__link" href="article.html">RaspberryPi - odpalamy diodę</a></h2>
-                    <p class="post__date">25 stycznia 2021 r.</p>
-                </header>
-                
-                <p>
-                    Dziś wielki dzień! Przystąpimy do pierwszej próby skomunikowania naszej malinki z zewnętrznym urządzeniem. Na pierwszy ogień pójdzie dioda LED. Przy okazji zainstalujemy Node.js oraz nauczymy się korzystać z npm.
-                </p>
-            </article> -->
-
     <?php endwhile; ?>
     <?php endif; ?>
 
